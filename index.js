e992bc24834fed1faef7f70714232aab4829150b
awsecops <96880169+awsecops@users.noreply.github.com>
2022-02-13 17:24:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
 
-  const msg = ' hi--- iam aws solution architect----aws---------------devops-sessions'
+  const msg = ' Aws and Devops-Sessions'
   res.end(msg);
 });
 
